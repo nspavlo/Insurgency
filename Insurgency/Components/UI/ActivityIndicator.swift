4282c1bbe585933d4ab30bb8ce74cc4c2deac6b1
Jans Pavlovs <jans.pavlovs@gmail.com>
2020-12-03 01:01:41 +0200

    Add wrapper for activity-indicator

---
+++ Insurgency/Components/UI/ActivityIndicator.swift
@@ -0,0 +1,33 @@
+//
+//  ActivityIndicator.swift
+//  Insurgency
+//
+//  Created by Jans Pavlovs on 03/12/2020.
+//
+
+import SwiftUI
+
+// MARK: Initialization
+
+struct ActivityIndicator {
+    let isAnimating: Bool
+    let style: UIActivityIndicatorView.Style
+}
+
+// MARK: UIViewRepresentable
+
+extension ActivityIndicator: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let spinner = UIActivityIndicatorView(style: style)
+        spinner.hidesWhenStopped = true
+        return spinner
+    }
+
+    func updateUIView(_ view: UIActivityIndicatorView, context: Context) {
+        if isAnimating {
+            view.startAnimating()
+        } else {
+            view.stopAnimating()
+        }
+    }
+}
